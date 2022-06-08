30ec1dc521e261ad9e6cce751e8bf5c9b6f03925
Elke Sleven <myprogrammersmail@gmail.com>
2022-06-08 03:15:12 +0200

    Domi__Linq
    console app met linq querys en IEnumerable objecten

---
+++ LINQ/LinqData.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic; //<-- voor gebruik te kunnen maken van een list 
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+/* // Data
+ * zet de class naar  --> public static 
+ * declareren van variable als public static  
+ */
+
+/*// opgaven 
+ * Gebruik de volgende IEnumerable objecten
+ * public static List<int> driehoek
+ * public static string[] tekst
+ 
+ */
+
+
+namespace LINQ
+{
+    public static class LinqData
+    {
+        public static List<int> driehoek = new List<int>()
+        {
+            1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,
+            1,   2,   3,   4,   5,   6,   7,   8,   9,   10,
+            1,   3,   6,   10,  15,  21,  28,  36,  45,
+            1,   4,   10,  20,  35,  56,  84,  120,
+            1,   5,   15,  35,  70,  126, 210,
+            1,   6,   21,  56,  126, 252,
+            1,   7,   28,  84,  210,
+            1,   8,   36,  120,
+            1,   9,   45,
+            1,   10,
+            1
+        };
+
+        public static string[] tekst = new string[]
+        {
+            "langspeelfilm", "vluchtmisdrijf", "dovemansgesprek", "containerpark",
+            "confituur", "inox", "valavond", "enerverend", "lopen", "bank", "vieruurtje",
+            "waterzooi", "maaltijdcheque", "arrondissement", "eindtermen", "nieuwjaarsbrief"
+        };
+
+
+
+
+
+
+      /*  public static List<LinqEntitie> linqEntitie = new List<LinqEntitie>
+        {
+            new LinqEntitie("Brug4", 0, "Arendonk", 2370, "Holstraat", 14, 3000, new List<int>{ 4, 3, 1, 5 }),
+            new LinqEntitie("Brug1", 1, "Arendonk", 2370, "Holstraat", 3, 8000, new List<int>{ 1, 4, 3, 5, 2, 3, 3, 4, 4}),
+            new LinqEntitie("Poort1", 2, "Gent", 9000, "Stropkaai", 12, 7000, new List<int>{ 5, 4, 3, 4 , 4}),
+            new LinqEntitie("Rijsteller", 3, "Hasselt", 3500, "Industrielaan", 1, 2500, new List<int>{ 5, 4, 3, 5, 2, 3, 4, 4}),
+            new LinqEntitie("Automa klein", 4, "Berchem", 2600, "Nieuwevaart", 77, 10000, new List<int>{ 4 }),
+            new LinqEntitie("Schuifla", 5, "Hasselt", 3500, "Industrielaan", 15, 1500, new List<int>{ 3, 5, 2 }),
+            new LinqEntitie("Automa groot", 6, "Berchem", 2600, "Nieuwevaart", 76, 30000, new List<int>{ 5 }),
+            new LinqEntitie("Brug2", 7, "Arendonk", 2370, "Molenweg", 7, 3000, new List<int>{ 4, 3, 5, 2 }),
+            new LinqEntitie("Veerhal", 8, "Melle", 9090, "Merelstraat", 48, 500, new List<int>{ 5, 5 }),
+            new LinqEntitie("Poort2", 9, "Gent", 9000, "Burgstraat", 113, 6600, new List<int>{ 1, 2, 1, 1, 2, 3}),
+            new LinqEntitie("D1", 10, "Knokke", 8300, "Vaart", 2, 2200, new List<int>{ 5, 4, 1 }),
+            new LinqEntitie("Brug3", 11, "Arendonk", 2370, "Molenweg", 8, 8000, new List<int>{ 5, 2, 3, 5, 5 }),
+            new LinqEntitie("D2", 12, "Knokke", 8300, "Vaart", 4, 2200, new List<int>{ 2, 3, 4 }),
+            new LinqEntitie("D3", 13, "Knokke", 8300, "Vaart", 6, 2200, new List<int>{ 3, 4, 3 })
+        };*/
+
+
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic; //<-- voor gebruik te maken van een list 
+using System.Linq;  // <-- voor linq 
+
+
+/*// opgaven oefeningen
+ * a. Druk al de woorden af in de array, tekst, die langer zijn dan tien karakters.
+ * b. Bereken het gemiddelde voor de getallen in de list driehoek die groter zijn dan 100.
+ * c. Druk al de unieke getallen af uit de List, driehoek.
+ * d. Druk alle unieke getallen af uit de List, driehoek, die deelbaar zijn door 
+ * drie of vijf. De getallen worden in een oplopende volgorde afgedrukt.
+ * e. Druk de woorden uit de array, tekst, af in een aflopende volgorde op
+ * basis van lengte. Verder zijn woorden met dezelfde lengte gesorteerd 
+ * op een oplopende alfabetische volgorde.
+ * f. Maak een groep op basis van de startletter van ieder woord voor al de woorden in de array, tekst.
+ * g. Vind alle paren van getallen en woorden waar de lengte van het woord
+ * uit de array, tekst, gelijk is aan een getal uit de List, driehoek. Druk het
+ * alle gevonden paren af.
+ * h. Maak een uitbreiding op de vorige oefening die er voor zorgt dat elk
+ * woord slechts één maal voorkomt in de resulterende paren.
+ * i. Druk alle woorden af uit de array, tekst, die minstens drie klinkers bevatten.
+ * 
+ 
+ 
+ */
+
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           // *a.Druk al de woorden af in de array, tekst, die langer zijn dan tien karakters.
+             SortLinq_a();
+
+           // *b.Bereken het gemiddelde voor de getallen in de list driehoek die groter zijn dan 100.
+             SortLinq_b();
+
+           // *c.Druk al de unieke getallen af uit de List, driehoek.
+             SortLinq_c();
+
+           // *d.Druk alle unieke getallen af uit de List, driehoek, die deelbaar zijn door drie of vijf. De getallen worden in een oplopende volgorde afgedrukt.
+             SortLinq_d();
+
+            //*e.Druk de woorden uit de array, tekst, af in een aflopende volgorde op basis van lengte. Verder zijn woorden met dezelfde lengte gesorteerd op een oplopende alfabetische volgorde.
+            SortLinq_e();
+
+            //*f.Maak een groep op basis van de startletter van ieder woord voor al de woorden in de array, tekst.
+            SortLinq_f();
+
+            //*g.Vind alle paren van getallen en woorden waar de lengte van het woord uit de array, tekst, gelijk is aan een getal uit de List, driehoek. Druk het alle gevonden paren af.
+            SortLinq_g();
+
+            //h.Maak een uitbreiding op de vorige oefening die er voor zorgt dat elk woord slechts één maal voorkomt in de resulterende paren.
+            SortLinq_h();
+
+            //i.Druk alle woorden af uit de array, tekst, die minstens drie klinkers bevatten.
+            SortLinq_i();
+    
+
+        }
+
+        //*a.Druk al de woorden af in de array 'tekst'  die langer zijn dan tien karakters.
+        public static void SortLinq_a()
+        {
+            // manier 1 query syntax
+            var querySyntax = from woord in LinqData.tekst where woord.Length > 10 select woord;
+            foreach (string woord in querySyntax)
+            {
+                Console.WriteLine(woord);
+
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+
+            // manier 2 Method syntax
+            var methodSyntax = LinqData.tekst.Where(x2 => x2.Length > 10);
+            foreach (string x2 in methodSyntax)
+            {
+                Console.WriteLine(x2);
+
+            }
+            Console.WriteLine("--------------------------------------------------------------------------------");
+            Console.ReadLine();
+        }
+
+        //* b.Bereken het gemiddelde voor de getallen in de list driehoek die groter zijn dan 100.
+        public static void SortLinq_b()
+        {
+            // manier 1 query syntax
+            var querySyntax = (from x in LinqData.driehoek where x > 100 select x).Average();
+            Console.WriteLine(querySyntax);
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+            // manier 2 Method syntax
+            var methodSyntax = LinqData.driehoek.Where(d => d > 100).Average();
+            Console.WriteLine(methodSyntax);
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+        }
+
+        //* c.Druk al de unieke getallen af uit de List, driehoek.
+        public static void SortLinq_c()
+        {
+            // manier 1 query syntax
+            var querySyntax = (from d in LinqData.driehoek select d).Distinct();
+            foreach (var item in querySyntax)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+
+
+            // manier 2 Method syntax
+            var methodSyntax = LinqData.driehoek.Distinct();
+            foreach (var item in methodSyntax)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+
+        }
+
+        // d.Druk alle unieke getallen af uit de List, driehoek, die deelbaar zijn door drie of vijf.
+        // De getallen worden in een oplopende volgorde afgedrukt.
+        public static void SortLinq_d()
+        {
+            // manier 1 query syntax
+            var querySyntax = LinqData.driehoek
+                .Where(d => d % 3 == 0 || d % 5 == 0)
+                .OrderBy(d => d)
+                .Distinct();
+
+            foreach (var item in querySyntax)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+          
+            // manier 2 Method syntax
+            var methodSyntax = LinqData.driehoek.Where(d => d % 3 == 0 || d % 5 == 0).OrderBy(d => d).Distinct();
+            foreach (var item in methodSyntax)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+        }
+
+        //e.Druk de woorden uit de array, tekst, af in een aflopende volgorde op
+        //basis van lengte.Verder zijn woorden met dezelfde lengte gesorteerd op een oplopende alfabetische volgorde.
+        public static void SortLinq_e()
+        {
+            // manier 1 query syntax
+            var qryMethod = from woord in LinqData.tekst orderby woord.Length descending, woord ascending select woord;
+            foreach (var item in qryMethod)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+
+            // manier 2 Method syntax
+            var methodSyntax = LinqData.tekst
+               .OrderBy(bee => bee)
+               .OrderByDescending(b => b.Length);
+            foreach (var item in methodSyntax)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+        }
+
+        //* f.Maak een groep op basis van de startletter van ieder woord voor al de woorden in de array, tekst.
+        public static void SortLinq_f()
+        {
+            // manier 1 query syntax
+
+            var querySyntax = from leukeNaam in LinqData.tekst
+                      orderby leukeNaam
+                      group leukeNaam by leukeNaam.First() into eersteLetter
+                      select eersteLetter;
+            foreach (var item in querySyntax)
+            {
+                Console.WriteLine($"{item.Key}");
+                foreach (var x in item)
+                {
+                    Console.WriteLine($"{x}");
+                }
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+
+            //  manier 2 Method syntax
+            var methodSyntax = LinqData.tekst
+                .OrderBy(t => t)
+                .GroupBy(x => x.First());
+            foreach (var item in methodSyntax)
+            {
+                Console.WriteLine($"{item.Key}");
+                foreach (var x in item)
+                {
+                    Console.WriteLine($"{x}");
+                }
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+        }
+
+        //g. Vind alle paren van getallen en woorden waar de lengte van het woord
+        //uit de array, tekst, gelijk is aan een getal uit de List, driehoek.Druk het alle gevonden paren af.
+        public static void SortLinq_g()
+        {
+            // manier 1 query syntax
+            var querySyntax = (from woord in LinqData.tekst
+                       join hoek in LinqData.driehoek
+                       on woord.Length equals hoek
+                       orderby woord.Length, woord
+                       select (woord, hoek));
+            foreach (var x in querySyntax)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+
+            // manier 2 Method syntax
+            var methodSyntax = LinqData.tekst
+                .Join(LinqData.driehoek, woord => woord.Length, hoek => hoek, (woord, hoek) => (hoek, woord));
+            foreach (var x in methodSyntax)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+
+        }
+
+        //h.Maak een uitbreiding op de vorige oefening die er voor zorgt dat elk
+        //woord slechts één maal voorkomt in de resulterende paren.
+        public static void SortLinq_h()
+        {
+            // manier 1 query syntax
+            var querySyntax = (from woord in LinqData.tekst
+                       join hoek in LinqData.driehoek
+                       on woord.Length equals hoek
+                       orderby woord.Length, woord
+                       select (woord, hoek)).Distinct();
+            foreach (var x in querySyntax)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+
+
+            // manier 2 Method syntax
+            var methodSyntax = LinqData.tekst
+                .Join(LinqData.driehoek, woord => woord.Length, hoek => hoek, (woord, hoek) => (hoek, woord))
+                .Distinct();
+            foreach (var x in methodSyntax)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+
+        }
+
+        //* i. Druk alle woorden af uit de array, tekst, die minstens drie klinkers bevatten.
+        public static void SortLinq_i()
+        {
+            char[] klinkers = { 'a', 'e', 'i', 'o', 'u' };
+            
+            // manier 1 query syntax
+            var querySyntax = from woord in LinqData.tekst
+                      where (from letter in woord
+                             where klinkers.Contains(letter)
+                             select letter).Count() >= 3
+                      select woord;
+            foreach (var x in querySyntax)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+
+
+            // manier 2 Method syntax
+            var methodSyntax = LinqData.tekst
+                .Where(woord => woord.Where(letter => klinkers.Contains(letter)).Count() >= 3);
+            foreach (var x in methodSyntax)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.ReadLine();
+        }
+
+
+    }
+
+}
